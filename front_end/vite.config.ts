70a519e65b4847bb6155780cedfe7dd4647217f4
Sicelo Mduna <siceloit@gmail.com>
2025-10-20 22:54:19 +0200

    updated vite.config.ts

--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  optimizeDeps: {
+    exclude: [
+      "@webstudio-is/sdk-components-react-router",
+      "@webstudio-is/sdk-components-react",
+      "@webstudio-is/sdk-components-react-radix"
+    ],
+  },
 });
